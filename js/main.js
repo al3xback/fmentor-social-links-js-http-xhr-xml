34462fa7a4a67468b625771faa8634ab20707343
alex <al3xback@gmail.com>
2024-02-10 18:37:01 +0700

    added space

--- js/main.js
+++ js/main.js
@@ -49,6 +49,7 @@ const renderCardContent = (data) => {
 
 	for (const socialLink of socialLinks) {
 		const { name, url } = socialLink;
+
 		const cardLinkTemplateNode = document.importNode(
 			cardLinkTemplate.content,
 			true
